d3147c4195f18bcddf677fab97d7f1f1fc47a620
Kevin Almansa <kevin.almansa@epitech.eu>
2015-01-30 13:35:29 +0100

    updated Doxygen

--- doc/class_api_new_year_1_1_i_socket.js
+++ doc/class_api_new_year_1_1_i_socket.js
@@ -4,5 +4,8 @@ var class_api_new_year_1_1_i_socket =
     [ "getHost", "class_api_new_year_1_1_i_socket.html#a39382074bf1356fe8249743a0885b0c3", null ],
     [ "getIp", "class_api_new_year_1_1_i_socket.html#aa7b741d5f9d698a1db19b0e40b166e63", null ],
     [ "getPort", "class_api_new_year_1_1_i_socket.html#a58f83e0d99158add7c271bbac47d3e04", null ],
-    [ "getSocket", "class_api_new_year_1_1_i_socket.html#a44ab596d025e78b39f75426c6381da5e", null ]
+    [ "getSocket", "class_api_new_year_1_1_i_socket.html#a44ab596d025e78b39f75426c6381da5e", null ],
+    [ "getSockInfo", "class_api_new_year_1_1_i_socket.html#abac104d1233dd68f5d128ecfda7a0e1e", null ],
+    [ "setSocket", "class_api_new_year_1_1_i_socket.html#a1fe56c5afbac22d5074e0ec7e6f51e38", null ],
+    [ "setSockInfo", "class_api_new_year_1_1_i_socket.html#a9acab95a129f0ef8d5edab3e8c406861", null ]
 ];
